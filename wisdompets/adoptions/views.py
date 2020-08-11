7a355b1052d9d422d6d5493287bbbb031a6b2e19
rajat.shrestha.pop <rajat.shrestha@eydean.com>
2020-08-11 23:11:40 +0545

    Simple views and urls

--- wisdompets/adoptions/views.py
+++ wisdompets/adoptions/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Pet
+
+
+def home(request):
+    return HttpResponse('<h1>Welcome home!</h1>')
+
+
+def pet_detail(request, pet_id):
+    return HttpResponse(f'<p>Pet id: </p><h1 style:"color:red">{pet_id}</h1>')
--- wisdompets/wisdompets/urls.py
+++ wisdompets/wisdompets/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+from adoptions import views
+
+
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
+    path('adoptions/<int:pet_id>/', views.pet_detail, name='pet_detail'),
 ]
