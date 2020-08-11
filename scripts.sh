df23060fbe440b97eca7efeb5f27cef7732d11de
rajat.shrestha.pop <rajat.shrestha@eydean.com>
2020-08-11 22:45:27 +0545

    Added admin creatin script with ORM python scripts

--- scripts.sh
+++ scripts.sh
@@ -23,3 +23,19 @@ python3 manage.py makemigrations
 python3 manage.py showmigrations
 python3 manage.py migrate
 
+
+# django admin
+python3 manage.py createsuperuser
+
+
+# Django ORM to query data
+python3 manage.py shell
+# >> from adoptions.models import Pet
+# >> Pet.objects.all()
+# >> pet1 = Pet.objects.all()[1]
+# >> print([(x.name, x.age for) x in Pet.objects.all()])
+# >> print(Pet.objects.get(id=4).name) 
+
+# .get method should not be used with non-unique field
+
+# >> Pet.objects.get(id=7).vaccinations.all()
