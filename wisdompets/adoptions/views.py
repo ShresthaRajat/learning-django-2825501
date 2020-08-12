f174b0dbc812d2838e929705504f1f5b8ae332ef
rajat.shrestha.pop <rajat.shrestha@eydean.com>
2020-08-12 16:59:58 +0545

    Added home and petdetail view

--- wisdompets/adoptions/views.py
+++ wisdompets/adoptions/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
 from .models import Pet
+from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('<h1>Welcome home!</h1>')
+    pets = Pet.objects.all()
+    return render(request, 'home.html', {
+        'pets': pets,
+    })
+    # return HttpResponse('<h1>Welcome home!</h1>')
 
 
 def pet_detail(request, pet_id):
-    return HttpResponse(f'<p>Pet id: </p><h1 style:"color:red">{pet_id}</h1>')
+    try:
+        pet = Pet.objects.get(id=pet_id)
+        #return HttpResponse(f'<p>Pet id: </p><h1 style:"color:red">{pet_id}</h1>')
+    except Pet.DoesNotExist:
+        raise Http404('Pet Not Found')
+    return render(request, 'pet_detail.html', {
+        'pet': pet,
+    })
